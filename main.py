ffcc9d04d50ed98e2a97a1be0a1c256fa1866ff8
Can Burak <canburakdnmz@gmail.com>
2023-01-24 23:30:08 +0300

    Update

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ Yapmak İstediğiniz İşlemi Giriniz
 4- Cümle Oyunu
 
 
+
 """)
 def c_listele():
     wb = load_workbook("Cümleler.xlsx")
@@ -40,7 +41,6 @@ def Cümle():
         if cevap == "q":
             break
         a = cevap.title()
-
         if a == b:
             puan = puan + 2
             print("*************")
@@ -58,8 +58,43 @@ def Cümle():
             print("Doğru Cevap: {}".format(b))
             print("*************")
     print("Oyun Bitti. Puanınız {}".format(puan))
+def KelimeTr():
+    wb = load_workbook("Words.xlsx")
+    ws = wb.active
+    words = {}
+    for i in range(2,ws.max_row+1):
+        en = ws["A{}".format(i)].value
+        tr = ws["B{}".format(i)].value
+        words[en] = tr
+    Hak = 5
+    puan = 0
+    while Hak>0:
+        c, b = random.choice(list(words.items()))
+        print("Sorunuz: {}".format(b))
+        cevap = input("Cevabı Giriniz: ")
+        if cevap == "q":
+            break
+        a = cevap.title()
 
-def Kelime():
+
+        if a == c:
+            puan = puan + 2
+            print("*************")
+            print("Tebrikler Doğru Cevap !!!")
+            print("*************")
+            print("Puanınız: {}".format(puan))
+            print("*************")
+
+        else:
+            print("Yanlıs Cevap")
+            print("*************")
+            Hak = Hak - 1
+            print("Kalan Hakkınız : {}".format(Hak))
+            print("*************")
+            print("Doğru Cevap: {}".format(c))
+            print("*************")
+    print("Oyun Bitti. Puanınız {}".format(puan))
+def KelimeEn():
     wb = load_workbook("Words.xlsx")
     ws = wb.active
     words = {}
@@ -109,7 +144,12 @@ while True:
     if islem == 1:
         k_listele()
     elif islem == 2:
-        Kelime()
+        islem2=input("Türkçe Çeviri / İngilizce Çeviri - (Tr)(En) ")
+        Sens= islem2.title()
+        if Sens == "En":
+            KelimeEn()
+        else:
+            KelimeTr()
     elif islem == 3:
         c_listele()
     elif islem == 4:
