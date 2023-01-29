5089edf1a912b5c47db0ca5fb7b5e7159e6db59b
Can Burak <canburakdnmz@gmail.com>
2023-01-29 13:44:46 +0300

    Update

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import random
-from openpyxl import Workbook,load_workbook
+from openpyxl import Workbook, load_workbook
+
 print("""
 İngilizce Kelime Oyununa Hoş Geldiniz
 Yarışma Boyunca 5 Hakkınız Bulunmaktadır 
 Her Doğru Cevap Hanenize 2 Puan Kazandıracaktır
 
 """)
+
+
 def c_listele():
     wb = load_workbook("Cümleler.xlsx")
     ws = wb.active
-    for satir in range(2,ws.max_row+1):
-        for sutun in range(1,ws.max_column+1):
-            print(" | "+str(ws.cell(satir,sutun).value) + " | ",end="")
+    for satir in range(2, ws.max_row + 1):
+        for sutun in range(1, ws.max_column + 1):
+            print(" | " + str(ws.cell(satir, sutun).value) + " | ", end="")
         print()
-def Cümle():
+
+
+def Cumle():
     wb = load_workbook("Cümleler.xlsx")
     ws = wb.active
     words = {}
-    for i in range(2,ws.max_row+1):
+    for i in range(2, ws.max_row + 1):
         en = ws["A{}".format(i)].value
         tr = ws["B{}".format(i)].value
         words[en] = tr
@@ -42,11 +47,13 @@ def Cümle():
             print("Doğrusu: {}".format(b))
             print("*************")
     print("Oyun Bitti. Puanınız {}".format(puan))
+
+
 def KelimeTr():
     wb = load_workbook("Words.xlsx")
     ws = wb.active
     words = {}
-    for i in range(2,ws.max_row+1):
+    for i in range(2, ws.max_row + 1):
         en = ws["A{}".format(i)].value
         tr = ws["B{}".format(i)].value
         words[en] = tr
@@ -71,11 +78,13 @@ def KelimeTr():
             print("Doğrusu: {}".format(b))
             print("*************")
     print("Oyun Bitti. Puanınız {}".format(puan))
+
+
 def KelimeEn():
     wb = load_workbook("Words.xlsx")
     ws = wb.active
     words = {}
-    for i in range(2,ws.max_row+1):
+    for i in range(2, ws.max_row + 1):
         en = ws["A{}".format(i)].value
         tr = ws["B{}".format(i)].value
         words[en] = tr
@@ -83,41 +92,44 @@ def KelimeEn():
     while True:
         c, b = random.choice(list(words.items()))
         print("Sorunuz: {}".format(c))
-        uppers=str(b)
+        uppers = str(b)
         cevap = input("Cevabı Giriniz: ")
         if cevap == "q":
             break
         if cevap.title() == uppers.title():
             puan = puan + 5
             print("*************")
-            print("Tebrikler Doğru Cevap !!!","Puanınız: {}".format(puan))
+            print("Tebrikler Doğru Cevap !!!", "Puanınız: {}".format(puan))
             print("*************")
 
         else:
             puan = puan - 5
-            print("Yanlıs Cevap","Puanınız: {}".format(puan))
+            print("Yanlıs Cevap", "Puanınız: {}".format(puan))
             print("*************")
             print("Doğrusu: {}".format(b))
             print("*************")
     print("Oyun Bitti. Puanınız {}".format(puan))
 
+
 def k_listele():
     wb = load_workbook("Words.xlsx")
     ws = wb.active
-    for satir in range(2,ws.max_row+1):
-        for sutun in range(1,ws.max_column+1):
-            print(" | "+str(ws.cell(satir,sutun).value) + " | ",end="")
+    for satir in range(2, ws.max_row + 1):
+        for sutun in range(1, ws.max_column + 1):
+            print(" | " + str(ws.cell(satir, sutun).value) + " | ", end="")
         print()
+
+
 while True:
     print("1-Kelime Listele | 2-Kelime Oyunu | 3-Cümle Listele | 4-Cümle Oyunu | q - Çıkış")
-    print("*"*70)
+    print("*" * 70)
 
     islem = input("Kararınız: ")
     if islem == "1":
         k_listele()
     elif islem == "2":
-        islem2=input("Türkçe Çeviri / İngilizce Çeviri - (Tr)(En) ")
-        Sens= islem2.title()
+        islem2 = input("Türkçe Çeviri / İngilizce Çeviri - (Tr)(En) ")
+        Sens = islem2.title()
         if Sens == "En":
             KelimeEn()
         else:
@@ -125,12 +137,4 @@ while True:
     elif islem == "3":
         c_listele()
     elif islem == "4":
-        Cümle()
-
-
-
-
-
-
-
-
+        Cumle()
